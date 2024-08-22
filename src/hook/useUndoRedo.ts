5484e5c0a388c6bdc4d6adf0f44e6a13ceb9c916
YRKlaptop <94chl@naver.com>
2024-08-22 16:57:11 +0900

    fix: undo redo localstorage

--- src/hook/useUndoRedo.ts
+++ src/hook/useUndoRedo.ts
@@ -2,11 +2,20 @@ import { RootState } from "@/store";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { setDrawables, setLayersNow } from "@/store/canvas";
 
+import useLocalStorage from "./useLocalStorage";
+import type { drawableInfoBufferType } from "@/utils/type";
+import { LocalStorageKey } from "@/utils/const";
+
 const useUndoRedo = () => {
   const dispatch = useDispatch();
   const { layersNow, layersHistory } = useSelector(
     (store: RootState) => store.canvas,
     shallowEqual
+  );
+
+  const [, setStoredDrawables] = useLocalStorage<drawableInfoBufferType>(
+    LocalStorageKey.drawables,
+    {}
   );
 
   const undo = () => {
@@ -16,9 +25,12 @@ const useUndoRedo = () => {
           ? layersHistory.length - 1
           : layersNow - 1
         : -1;
+    const newDrawables = prevIndex > -1 ? layersHistory[prevIndex] : {};
 
     dispatch(setLayersNow(prevIndex));
-    dispatch(setDrawables(prevIndex > -1 ? layersHistory[prevIndex] : {}));
+    dispatch(setDrawables(newDrawables));
+
+    setStoredDrawables(newDrawables);
   };
 
   const redo = () => {
@@ -26,9 +38,11 @@ const useUndoRedo = () => {
       layersNow < layersHistory.length - 1
         ? layersNow + 1
         : layersHistory.length - 1;
+    const newDrawables = nextIndex > -1 ? layersHistory[nextIndex] : {};
 
     dispatch(setLayersNow(nextIndex));
     dispatch(setDrawables(nextIndex > -1 ? layersHistory[nextIndex] : {}));
+    setStoredDrawables(newDrawables);
   };
 
   return { undo, redo };
