94c32ab526f82ccce29c7c8ce346851dff03a894
YRKlaptop <94chl@naver.com>
2024-08-23 16:57:13 +0900

    fix: useTransform attrs

--- src/hook/useTransformDrawable.ts
+++ src/hook/useTransformDrawable.ts
@@ -1,10 +1,20 @@
 import { RootState } from "@/store";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { setDrawable } from "@/store/canvas";
+import { isUndefined } from "underscore";
 
 import useAddLayerHistory from "./useAddLayerHistory";
+import { drawablePointsType } from "@/utils/type";
 
 type Props = { id: string };
+
+export type transformInfoType = {
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
+  points?: drawablePointsType;
+};
 
 const useTransformDrawable = ({ id }: Props) => {
   const dispatch = useDispatch();
@@ -15,18 +25,24 @@ const useTransformDrawable = ({ id }: Props) => {
 
   const addLayerHistory = useAddLayerHistory();
 
-  const trasnformDrawable = (transformedInfo: {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-  }) => {
+  const transformDrawable = ({
+    x,
+    y,
+    width,
+    height,
+    points,
+  }: transformInfoType) => {
     const targetDrawable = drawables[id];
     if (!targetDrawable) return;
-    const newDrawable = {
-      ...structuredClone(targetDrawable),
-      ...transformedInfo,
-    };
+
+    const newDrawable = structuredClone(targetDrawable);
+
+    if (!isUndefined(x)) newDrawable.x = x;
+    if (!isUndefined(y)) newDrawable.y = y;
+    if (!isUndefined(width)) newDrawable.width = width;
+    if (!isUndefined(height)) newDrawable.height = height;
+    if (!isUndefined(points)) newDrawable.points = points;
+
     dispatch(setDrawable(newDrawable));
 
     const newDrawables = structuredClone(drawables);
@@ -35,7 +51,7 @@ const useTransformDrawable = ({ id }: Props) => {
     addLayerHistory(newDrawables);
   };
 
-  return trasnformDrawable;
+  return transformDrawable;
 };
 
 export default useTransformDrawable;
